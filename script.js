621688c0b9b8fd74dea702de1482dd4e9e7ff93a
sheilla mungai <mungaisheilla19@gmail.com>
2025-05-11 20:26:46 +0300

    added commits

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const navbar = document.getElementById('navbar')
-element.addEventListener("click",(event) =>{
+navbar.addEventListener("click",(event) =>{
     if (event.target.tagName === 'A'){
         event.preventDefault();
         const targetId = event.target.getAttribute('href').substring(1);
@@ -11,5 +11,19 @@ element.addEventListener("click",(event) =>{
     }
 }
 );
+const buyNowButton =document.getElementById("buy")
+    buyNowButton.addEventListener('click', (event)=>{
+        event.preventDefault();
+            const menuSection = document.getElementById('menu');
+
+     if (menuSection){
+        menuSection.scrollIntoView({
+            behaviour: 'smooth'
+        });
+     }
+        
+    }
+    )
+
 
 
